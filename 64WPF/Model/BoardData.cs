376cea213627916cafefe13d1a6a319042b63944
Alcthulu <33372285+Alcthulu@users.noreply.github.com>
2019-11-10 13:17:03 +0100

    Data types
    Creados los modelos para las fichas y el tablero.

---
+++ 64WPF/Model/BoardData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _64WPF.Model
+{
+    class BoardData
+    {
+        public NumberChip[][] board;
+
+        public BoardData(int size)
+        {
+            board = new NumberChip[size][];
+            for(int i = 0; i < size; i++)
+            {
+                board[i] = new NumberChip[size];
+                for(int j = 0; j < size; j++)
+                {
+                    board[i][j] = new NumberChip(0);
+                }
+            }
+        }
+
+        public NumberChip[][] getBoard()
+        {
+            return board;
+        }
+
+        public void setBoard(NumberChip[][] b)
+        {
+            board = b;
+        }
+
+    }
+}
---
+++ 64WPF/Model/NumberChip.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _64WPF.Model
+{
+    public class NumberChip
+    {
+        int value;
+
+        public NumberChip(int v)
+        {
+            value = v;
+        }
+        public void setValue(int v)
+        {
+            value = v;
+        }
+        public int getValue()
+        {
+            return value;
+        }
+
+        
+    }
+}
